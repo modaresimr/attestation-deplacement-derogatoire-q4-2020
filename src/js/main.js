9849ff69a32e685edcd4d87372685a29123d0a68
Ali <modaresi.mr@gmail.com>
2020-11-01 20:47:05 +0100

    fix lint

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,8 @@ import './icons'
 import './check-updates'
 import { prepareForm } from './form-util'
 import { warnFacebookBrowserUserIfNecessary } from './facebook-util'
-import { addVersion } from './util'
+import { addVersion , autoFill } from './util'
 import { createForm } from './form'
-import { autoFill } from './util'
 
 
 warnFacebookBrowserUserIfNecessary()

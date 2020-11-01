f7ca9474c89f5419b0f4de9c7e55408a3983190e
modaresimr <9498182+modaresimr@users.noreply.github.com>
2020-11-01 19:50:31 +0000

    Deploying to pages from  @ 36d4ce0d757adbf41bf820777a096b9f1c980d2f 🚀

---
+++ sw.js
@@ -0,0 +1,129 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "android-chrome-192x192.0fcebb82.png",
+    "revision": "71c32dc8b4da4840d61a1796a2bc230b"
+  },
+  {
+    "url": "android-chrome-512x512.ae40f73d.png",
+    "revision": "8bf8012c4f4ae251d660b4739171b7ed"
+  },
+  {
+    "url": "apple-touch-icon-120x120-precomposed.044c6f3f.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-120x120.044c6f3f.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-precomposed.fdeabd24.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "apple-touch-icon.fdeabd24.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "certificate.d1673940.pdf",
+    "revision": "624e7a3f5560db68d93eda0129cc2922"
+  },
+  {
+    "url": "confidentialite.44e86f13.css",
+    "revision": "97eebbff686b6cae7e6406aaf6a0e0b0"
+  },
+  {
+    "url": "confidentialite.47111068.js",
+    "revision": "1ad7c4acfdfbeef7438fe7150738fd80"
+  },
+  {
+    "url": "confidentialite.html",
+    "revision": "dfad43fbf259a7b808c19633bbfdbd8d"
+  },
+  {
+    "url": "favicon-16x16.a4687270.png",
+    "revision": "652605ae0182d51781ab0be639ad2bda"
+  },
+  {
+    "url": "favicon-32x32.623384d0.png",
+    "revision": "674c9c4ef1e4c7ea9de1218ee0bfd0cf"
+  },
+  {
+    "url": "index.html",
+    "revision": "32a4cd87c6fcfb9a66f127180775dd21"
+  },
+  {
+    "url": "logo_dnum_dark.0fe33c5b.svg",
+    "revision": "da8bdc57d4f231585216c53da752d00a"
+  },
+  {
+    "url": "logo_dnum.19ebc682.svg",
+    "revision": "3a41bfa41e4671414da29db168c37d66"
+  },
+  {
+    "url": "main.44e86f13.css",
+    "revision": "5d850aca089e8233d1d9d4abfa1b139a"
+  },
+  {
+    "url": "main.cba86d2d.js",
+    "revision": "77ab146f963391d33227bef861280442"
+  },
+  {
+    "url": "marianne-bold-webfont.1505950c.woff2",
+    "revision": "e67f6cefe32cc39f909e605c8d6337a9"
+  },
+  {
+    "url": "marianne-bold-webfont.7424dbde.woff",
+    "revision": "0bcc99dd4adfb78e11098fedfe531cbb"
+  },
+  {
+    "url": "marianne-regular-webfont.0a959359.woff",
+    "revision": "89f4f2326c77429e98693cf83703face"
+  },
+  {
+    "url": "marianne-regular-webfont.daa94941.woff2",
+    "revision": "d2c09e5f58d8360f541e2a8726c33587"
+  },
+  {
+    "url": "MIN_Interieur_RVB_dark.0e5ee525.svg",
+    "revision": "345794cee228a40837ab654184cd2c96"
+  },
+  {
+    "url": "MIN_Interieur_RVB.124e26ea.svg",
+    "revision": "6823b6d87f43d208b17ff81e178f9ae9"
+  },
+  {
+    "url": "safari-pinned-tab.1551797e.svg",
+    "revision": "f53452e6ac8760f12bab91672e91d39b"
+  },
+  {
+    "url": "./",
+    "revision": "bb3a80b6ba911a28aa168a4ccd63f21c"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://modaresimr.github.io/attestation-deplacement-derogatoire-q4-2020//index.html"));
